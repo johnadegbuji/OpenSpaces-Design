0a615bad75bb9494ab43d97e18a361b1e667e830
johnadegbuji <johnadegbuji1@gmail.com>
2022-05-26 11:22:20 -0700

    removed twitter analytics stuff

--- pages/index.js
+++ pages/index.js
@@ -50,14 +50,6 @@ export default function Home() {
         <meta property="og:title" content="OPENSPACES" />
         <meta property="og:url" content="https://www.openspaces.design" />
         <meta property="og:image" content="/socialsharelogo.png" />
-  
-        <script>
-        !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-        },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
-        a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-        twq('init','o8wkr');
-        twq('track','PageView');
-        </script>
       </Head>
       <HeroSection>
         <Navbar />
